a77e95da918fb52759b578588e5c7bef86735ec6
Ricky Andrianto Kusuma <ricky.ad58@gmail.com>
2024-08-22 22:58:35 +0700

    Fix(Navbar): Fix UI Navbar on Mozilla Browser

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -58,7 +58,7 @@ const Navbar = (props) => {
 
       {/* Search Group */}
       <div className="relative mt-[3px] flex h-[61px] w-[355px] flex-grow items-center justify-around gap-2 rounded-full bg-white px-2 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:w-[365px] md:flex-grow-0 md:gap-1 xl:w-[365px] xl:gap-3">
-        <div className="flex h-full items-center rounded-full bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[225px]">
+        <div className="flex h-full items-center rounded-full bg-lightPrimary text-navy-700 dark:bg-navy-900 dark:text-white xl:w-[200px]">
           <p className="pl-3 pr-2 text-xl">
             <FiSearch className="h-5 w-5 text-gray-400 dark:text-white" />
           </p>
